88e6016b0eab5c71dd62ed0d6c172eb42336cfc0
victor ciechovicz <victor09ciechovicz@outlook.com>
2022-11-11 20:34:42 -0300

    :sparkles: feat: Navegacao para Screen SignUp

--- app/src/main/java/com/example/mathschool/SignIn.java
+++ app/src/main/java/com/example/mathschool/SignIn.java
@@ -1,10 +1,18 @@
 package com.example.mathschool;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignIn extends AppCompatActivity {
+
+    private AppCompatButton button_signup;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +20,20 @@ public class SignIn extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
 
         getSupportActionBar().hide();
+
+button_signup =findViewById(R.id.appCompatButtonSingUp);
+
+button_signup.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(SignIn.this, SignUp.class);
+    startActivity(intent);
     }
-}
+});
+
+
+    }
+
+
+
+    }
